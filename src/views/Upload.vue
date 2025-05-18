897d276efe914a78d50230c53f2c7effa2be3d9f
Alex Wheeldon <eawheeldon@gmail.com>
2025-05-18 09:36:29 -0700

    Add a bunch of changes

---
+++ src/views/Upload.vue
@@ -0,0 +1,10 @@
+<!-- src/views/Upload.vue -->
+<template>
+    <div class="p-6">
+        <h1 class="text-2xl font-bold">Upload</h1>
+        <p>This is the upload page.</p>
+
+        <lable for="upload">Chose a file to upload</lable>
+        <input type="file" id="upload" accept=".doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document" />
+    </div>
+</template>
